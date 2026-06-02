73e4f8e31ca96800e38b36cff3aa0e64adb3a8dc
Yashdeep Raj <yashdeep.raj@thoughtworks.com>
2026-06-02 11:11:50 +0530

    chore: update eslint configuration and dependencies, add .gitignore
    - Updated eslint and eslint-plugin-react to latest versions.
    - Changed lint script to target src directory directly.
    - Added globals dependency for better global variable handling.
    - Created eslint.config.js for improved ESLint configuration.
    - Added .gitignore to exclude node_modules and dist directories.

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs/cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
+]);
